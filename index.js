e905787d41019ce455904cd790cee00207855bd9
Brandon Dugan <72161175+Dugan94@users.noreply.github.com>
2021-02-12 17:39:22 -0800

    Updated JS
    I got the chance to practice and reduce my code so it would not repeat.

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-/*let button = document.getElementsByTagName("button")[0]
-
-button.addEventListener("mouseenter", function () {
-    console.log("CLICK!!!!");
-})*/
 
 let button = document.getElementById("enter");
 let input = document.getElementById("userinput");
 let ul = document.querySelector("ul");
 
-button.addEventListener("click", function () {
-   let li = document.createElement("li");
-   li.appendChild(document.createTextNode(input.value));
-   ul.appendChild(li);
-})
+function inputLength() {
+    return input.value.length;
+}
+
+function createListElement () {
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li); 
+    input.value = "";
+}
+
+function addListAfterClick() {
+    if (inputLength() > 0) {
+        createListElement();  
+    }
+}
+
+function addListAfterKeypress(event) {
+        if(inputLength () > 0 && event.keyCode === 13) {
+            createListElement();
+        }
+    }
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress)
+   
